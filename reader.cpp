b280f684575f55adfb2e6a31d1809e27f468eaab
Makarov Vyacheslav <orchara69@gmail.com>
2022-10-26 03:04:58 +0500

    version 0.5

--- reader.cpp
+++ reader.cpp
@@ -2,40 +2,48 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <functional>
 
-enum class sort_order{
-    NAME = 1,
-    SURNAME = 2,
-    P_NUMBER = 3
-};
 
 typedef struct PB{
     std::string name;
     std::string surname;
     std::string p_number;
+
+    std::string to_string(std::string order_by){        
+        if(order_by == "surname"){
+            return surname + " " + name + ": " + p_number;
+        }
+        if(order_by == "phone"){
+            return p_number + ": " + name + " " + surname;
+        }
+        return name + " " + surname + ": " + p_number;
+    }
 } PB;
 
-bool by_name(PB a, PB b){
-	return a.name < b.name;
+
+std::function<bool (PB, PB)> compare(std::string sort_by){    
+    if(sort_by == "surname"){
+        return [](PB a, PB b){return a.surname < b.surname;};
+    }
+    if(sort_by == "phone"){
+        return [](PB a, PB b){return a.p_number < b.p_number;};
+    }
+    return [](PB a, PB b){return a.name < b.name;};
 }
 
-bool by_surname(PB a, PB b){
-	return a.surname < b.surname;
+void print_help(){
+    std::cout << "Usage:\nreader <path> <sort_option>\n\n";
+    std::cout << "path - absolute file path to the file to be read\n";
+    std::cout << "sort options:\n";
+    std::cout << "name - sort by name (default)\n";
+    std::cout << "surname - sort by surname\n";
+    std::cout << "phone - sort by phone number\n";
 }
-
-bool by_phone(PB a, PB b){
-	return a.p_number < b.p_number;
-}
-
 
 int main(int argc, char* argv[]){
     if(argc < 2 || argc > 3){
-        std::cout << "Usage:\nreader <path> <sort_option>\n\n";
-        std::cout << "path - absolute file path to the file to be read\n";
-        std::cout << "sort options:\n";
-        std::cout << "1 / name - sort by name(default)\n";
-        std::cout << "2 / surename - sort by surname\n";
-        std::cout << "3 / phone - sort by phone number\n";
+        print_help();
         return 0;
     }
     std::string path = argv[1];
@@ -44,7 +52,13 @@ int main(int argc, char* argv[]){
     std::vector<PB> lst;
     
     if(argc == 3){
-    	sort_by = argv[2];
+        std::string temp = argv[2];
+        if(temp != "name" && temp != "surname" && temp != "phone"){
+            std::cerr << "not valid sort_option\n\n";
+            print_help();
+            exit(1);
+        }
+        sort_by = argv[2];
     }    
     fs.open(path);
     if(!fs){
@@ -56,8 +70,9 @@ int main(int argc, char* argv[]){
         name.pop_back();
     	lst.push_back({name, surname, phone});
     }
-    std::sort(lst.begin(), lst.end(), by_phone);
+    
+    std::sort(lst.begin(), lst.end(), compare(sort_by));
     for(auto x : lst){
-    	std::cout << x.name << " " << x.surname << ": " << x.p_number << std::endl;
+    	std::cout << x.to_string(sort_by) << std::endl;
     }
 }

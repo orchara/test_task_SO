bae2495300611565ca93e7bba98800b0e3e9a510
Makarov Vyacheslav <orchara69@gmail.com>
2022-10-26 08:50:03 +0500

    version 0.6

--- reader.cpp
+++ reader.cpp
@@ -10,14 +10,18 @@ typedef struct PB{
     std::string surname;
     std::string p_number;
 
-    std::string to_string(std::string order_by){        
+    std::string to_string(std::string order_by){
         if(order_by == "surname"){
             return surname + " " + name + ": " + p_number;
         }
         if(order_by == "phone"){
             return p_number + ": " + name + " " + surname;
         }
-        return name + " " + surname + ": " + p_number;
+        if(order_by == "name"){
+            return name + " " + surname + ": " + p_number;
+        }
+        return surname + " " + name + ": " + p_number;
+        
     }
 } PB;
 
@@ -29,40 +33,44 @@ std::function<bool (PB, PB)> compare(std::string sort_by){
     if(sort_by == "phone"){
         return [](PB a, PB b){return a.p_number < b.p_number;};
     }
-    return [](PB a, PB b){return a.name < b.name;};
+    if(sort_by == "name"){
+        return [](PB a, PB b){return a.name < b.name;};
+    }
+    return nullptr;
 }
 
 void print_help(){
-    std::cout << "Usage:\nreader <path> <sort_option>\n\n";
-    std::cout << "path - absolute file path to the file to be read\n";
+    std::cout << "Usage:\nreader <path> [sort_option]\n\n";
+    std::cout << "path - file path to the file to be read\n";
     std::cout << "sort options:\n";
-    std::cout << "name - sort by name (default)\n";
+    std::cout << "name - sort by name\n";
     std::cout << "surname - sort by surname\n";
     std::cout << "phone - sort by phone number\n";
 }
 
 int main(int argc, char* argv[]){
-    if(argc < 2 || argc > 3){
+    if(argc < 2 || argc > 3){        
         print_help();
-        return 0;
+        exit(1);
     }
     std::string path = argv[1];
-    std::string sort_by = "name";
+    std::string sort_by = "def";
     std::fstream fs;
     std::vector<PB> lst;
     
     if(argc == 3){
-        std::string temp = argv[2];
-        if(temp != "name" && temp != "surname" && temp != "phone"){
+        sort_by = argv[2];
+        if(sort_by != "name" && sort_by != "surname" && sort_by != "phone"){
             std::cerr << "not valid sort_option\n\n";
             print_help();
             exit(1);
         }
-        sort_by = argv[2];
+        
     }    
     fs.open(path);
     if(!fs){
-    	std::cout << "file not found\nget valid file path\n";
+    	std::cerr << "file not found\nget valid file path\n";
+        exit(1);
     }    
     while(!fs.eof()){
     	std::string name, surname, phone;
@@ -70,9 +78,11 @@ int main(int argc, char* argv[]){
         name.pop_back();
     	lst.push_back({name, surname, phone});
     }
-    
-    std::sort(lst.begin(), lst.end(), compare(sort_by));
+    if(sort_by != "def"){
+        std::sort(lst.begin(), lst.end(), compare(sort_by));
+    }
     for(auto x : lst){
     	std::cout << x.to_string(sort_by) << std::endl;
     }
+    return 0;
 }
